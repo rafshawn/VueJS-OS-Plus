96b081d24b77ae42e5738fa3874de4842c497ff1
Shawn <rafshawn@yandex.com>
2026-03-04 23:33:58 -0500

    annoying slider

--- src/components/macos/ControlCenter.vue
+++ src/components/macos/ControlCenter.vue
@@ -45,6 +45,7 @@ export default {
 </script>
 
 <template>
+<Teleport to="#app">
 <div
     v-if="isOpen"
     class="dropdown-menu"
@@ -94,25 +95,72 @@ export default {
             </div>
         </div>
         <div class="panel small">
-            <Rectangle class="main-icon" />
+            <div class="stage-manager">
+                <div class="box-stack">
+                    <div class="box"></div>
+                    <div class="box"></div>
+                    <div class="box"></div>
+                </div>
+                <Rectangle class="main-icon" />
+            </div>
             <div class="subtext">Stage Manager</div>
         </div>
         <div class="panel small">
             <RectangleOnRectangle class="main-icon" />
             <div class="subtext">Screen Mirroring</div>
         </div>
-        <div class="panel full">Display</div>
-        <div class="panel full">Sound</div>
-        <div class="panel full">Music</div>
+        <div class="panel full">
+            <div class="slider-box">
+                Display
+                <input
+                    type="range"
+                    min="0"
+                    max="100"
+                    value="80"
+                    class="slider"
+                    id="displayRange"
+                >
+            </div>
+        </div>
+        <div class="panel full">
+            <div class="slider-box">
+                Sound
+                <div class="sound">
+                    <input
+                    type="range"
+                    min="1"
+                    max="100"
+                    value="80"
+                    class="slider"
+                    id="soundRange"
+                    >
+                    <div class="cc-icon-slider">
+                        <DarkMode class="main-icon"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="panel full music">
+            Music
+        </div>
     </div>
 </div>
+</Teleport>
 </template>
 
 <style scoped>
 .dropdown-menu {
+    background: var(--bg-clear);
     padding: 0;
     min-width: 300px;
+    max-width: 300px;
+    border: 1px hidden var(--border-light);
     border-radius: 16px;
+}
+
+html.dark-mode .dropdown-menu {
+    background: var(--bg-clear-dark);
+    border: 1px solid var(--border-dark);
 }
 
 .container {
@@ -127,11 +175,16 @@ export default {
     align-items: center;
     justify-content: center;
     gap: 10px;
-    background-color: yellow;
+    background-color: rgba(255, 255, 255, 0.3);
     border-radius: 12px;
-    font-size: 30px;
-    /* box-shadow: 0 8px 1px rgba(0, 0, 0, 0.2); */
-    border: 1px solid
+    font-size: 12px;
+    font-weight: 500;
+    box-shadow: 0 1px 6px rgba(0, 0, 0, 0.2), 1px 14px 14px rgba(0, 0, 0, 0.2);
+}
+
+html.dark-mode .panel {
+    background-color: transparent;
+    /* border: 1px solid rgba(125, 125, 125, 0.5);; */
 }
 
 .panel .item {
@@ -142,14 +195,19 @@ export default {
 
 .panel.small {
     flex-direction: column;
+    padding: 8px 10px;
+    gap: 4px;
+    text-align: center;
+}
+
+.panel.small .main-icon {
+    width: 20px;
+    height: 20px;
 }
 
 .panel.wide {
     grid-column: span 2;
-}
-
-.panel.wide .item {
-    margin: 10px;
+    padding: 16px 10px;
 }
 
 .panel.medium {
@@ -171,6 +229,7 @@ export default {
 }
 
 .panel.full {
+    padding: 8px 10px;
     grid-column: span 4;
 }
 
@@ -178,32 +237,38 @@ export default {
     flex-grow: 1;
 }
 
-
 .subtext {
     font-size: 11px;
     color: var(--system-font-2);
-    width: 30%;
 }
 
-.item {
-    font-size: 12px;
-    font-weight: 500;
-}
-
-.item .cc-icon {
-    padding: 4px;
+.cc-icon {
+    padding: 6px;
     margin-right: 8px;
     border-radius: 50%;
     display: flex;
     align-items: center;
-    background-color: white;
-    border: 1px solid black;
-    color: var(--system-font);
+    background-color: var(--cc-icon-bg);
+    color: var(--cc-icon);
+}
+
+.cc-icon-slider {
+    padding: 6px;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    background-color: var(--cc-icon-bg);
+    color: var(--cc-icon);
+}
+
+html.dark-mode .cc-icon,
+html.dark-mode .cc-icon-slider {
+    background-color: var(--cc-icon-bg);
 }
 
 .main-icon {
-    width: 18px;
-    height: 18px;
+    width: 16px;
+    height: 16px;
 }
 
 .chev-r-icon {
@@ -211,13 +276,73 @@ export default {
     height: 14px;
 }
 
-.item-label {
-    flex-grow: 1;
+.stage-manager {
+    display: flex;
+    align-items: center;
+    gap: 3px;
 }
 
-.item-shortcut {
-    color: #999;
-    font-size: 12px;
-    margin-left: 20px;
+.box-stack {
+    display: flex;
+    flex-direction: column;
+    gap: 2px;
 }
+
+.box {
+    width: 4px;
+    height: 4px;
+    background-color: var(--system-font);
+}
+
+.slider-box {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
+    gap: 6px;
+}
+
+.slider-box .sound {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+}
+
+.slider {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 100%;
+    height: 18px;
+    border-radius: 18px;
+    border: 1px solid var(--grey-btn-border);
+    cursor: pointer;
+    overflow: hidden;
+    background: green;
+}
+
+.slider::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 18px;
+    height: 18px;
+    background: white;
+    border: 1px solid var(--grey-btn-border);
+    border-radius: 50%;
+    box-shadow: -300px 0 0 300px red;
+}
+
+.slider::-moz-range-thumb {
+    width: 18px;
+    height: 18px;
+    background: blue;
+    border: 1px solid var(--grey-btn-border);
+    border-radius: 50%;
+    /* box-shadow: -300px 0 0 300px red; */
+}
+
+.slider::-moz-range-progress {
+    height: 18px;
+    background: red;
+}
+
 </style>
